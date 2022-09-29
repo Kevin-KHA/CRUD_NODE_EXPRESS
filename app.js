b031f1201cc67fc93dc7569ba23cb23b9a297664
Kevin-KHA <201378@ids.upchiapas.edu.mx>
2022-09-28 20:37:19 -0500

    configuracion CORS

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
