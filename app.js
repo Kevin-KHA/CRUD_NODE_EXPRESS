318ad0ecc99779ba3c185f6543c531ae4d1480df
Kevin-KHA <201378@ids.upchiapas.edu.mx>
2022-09-28 20:45:22 -0500

    configuracion CORS 2.0

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+const cors = require("cors");
 
-
+app.use(cors());
 
 var createError = require('http-errors');
 var express = require('express');
